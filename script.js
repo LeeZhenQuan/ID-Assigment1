148eefdb415bec1125341e912e03b10bf2eb8787
4python-github <github4python@gmail.com>
2021-12-01 19:40:24 +0800

    added in js for button

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello Dear Visitor! Thank you for email me I wil get back to you shortly </br>"}
+    document.getElementById("demo").innerHTML = "Hello Dear Visitor! Thank you for sending the email I wil get back to you shortly </br>"}
