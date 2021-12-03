fc6289a40a209e60c802a264c1cb4a53e153e070
4python-github <github4python@gmail.com>
2021-12-03 17:54:10 +0800

    new fuction sendEmail to open mailbox with name

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,7 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Hello Dear Visitor! Thank you for sending the email I wil get back to you shortly </br>"}
+    
+function sendEmail() 
+    {
+        window.location = "mailto:Damon0lee@gmail.com";
+    }
